8983f74354a4fb4cb135462e5dfc8d5aab3b5b5d
Cristian <soilitacristian@gmail.com>
2023-10-31 20:41:55 +0000

    Initial commit

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Blender</li>
+                <li>3ds Max</li>
+                <li>Unreal Engine</li>
+                <li>Unity</li>
+            </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>LCUCK</li>
+                <li>CEITI</li>
+            </ul>
+        ),
+    },
+];
+
+const AboutSection = () => {
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
+
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+
+    return (
+        <section className="text-white" id="about">
+                            <span className="text-4xl font-bold text-white mb-4">Previous Project</span>
+            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+                <Image src="/images/IslandBottle.png" width={1000} height={1000} />
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+                    <p className="text-base lg:text-lg">
+                        I am a college student at LCUCK following a CMP (Game Development) course.
+                        With a passion for computers and a drive to learn, I have explored programming, different jobs, and 3D software over the years.
+                        I finally found my calling as a 3D artist using Blender, and I am eager to develop my skills and pursue a career in this field.
+                        <br></br>
+                        <br></br>
+                        I am a highly motivated and results-oriented individual with a strong work ethic. I am also a quick learner and I am always looking for new ways
+                        to improve my skills. I am also familiar with other 3D software such as 3ds Max. I am also familiar with game engines such as Unity
+                        and Unreal Engine.
+
+
+                    </p>
+                    <div className="flex flex-row justify-start mt-8">
+                        <TabButton
+                            selectTab={() => handleTabChange("skills")}
+                            active={tab === "skills"}
+                        >
+                            {" "}
+                            Skills{" "}
+                        </TabButton>
+                        <TabButton
+                            selectTab={() => handleTabChange("education")}
+                            active={tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                    </div>
+                    <div className="mt-8">
+                        {TAB_DATA.find((t) => t.id === tab).content}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutSection;
---
+++ app/components/ProjectsSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Chibi Bat",
+    description: "Used multiple modifiers and a particle system to create the hair for this chibi bat, along with multiple lights to create a scene for rendering",
+    image: "/images/bat.png",
+    tag: ["All", "Blender"],
+  },
+  {
+    id: 2,
+    title: "Axe",
+    description: "Used a sketch as a reference that showed both the front and the side, it was easy to make by using extrude and move tool",
+    image: "/images/Axe.png",
+    tag: ["All", "Blender"],
+  },
+  {
+    id: 3,
+    title: "Chibi Owl",
+    description: "One of the first models I made without following a tutorial",
+    image: "/images/Owl.png",
+    tag: ["All", "Blender"],
+  },
+  {
+    id: 4,
+    title: "Sword",
+    description: "It's got a pretty cool bloom",
+    image: "/images/SwordBlend.png",
+    tag: ["All", "Blender"],
+  },
+  {
+    id: 5,
+    title: "Bottle Island",
+    description: "An island in a bottle? Who would have thought, does anyone live in there?",
+    image: "/images/IslandBottle1080.png",
+    tag: ["All", "Blender"],
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Blender"
+          isSelected={tag === "Blender"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
