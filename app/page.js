17f05d64a0006ad89ac5a55d3d8d4cdafb833b72
Cristian <soilitacristian@gmail.com>
2023-11-01 12:54:32 +0000

    title

--- app/page.js
+++ app/page.js
@@ -8,6 +8,9 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Head>
+        <title>Portfolio</title>
+      </Head>
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
@@ -17,5 +20,6 @@ export default function Home() {
       </div>
       <Footer />
     </main>
+
   );
 }
