648651368b7787cb0a327d086a8366f2e18ee25a
Cristian <soilitacristian@gmail.com>
2023-11-01 12:56:18 +0000

    title change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-
+import Head from "next/head";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
